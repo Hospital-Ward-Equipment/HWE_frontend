6e52e6469050cfa53649d9cee23c3af57e3ae652
akilaanjanadissanayaka <akiladissanayaka255@gmail.com>
2022-12-27 11:37:16 +0530

    add child classes

---
+++ src/views/components/SelectBox.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-row align="center" class="slectBox">
+      <v-col
+        class="d-flex"
+        cols="12"
+        sm="6"
+      >
+        <v-select
+        item-text="name"
+        item-value="id"
+        v-model="defaultSelected"
+          :items="product"
+          solo
+        ></v-select>
+      </v-col>
+    </v-row>
+  </template>
+  <script>
+    export default{
+        data: () => ({
+            defaultSelected: {
+      id: 1,
+      name: "All"
+    },
+    product: [
+      {
+        id: 1,
+        name: "All"
+      },
+      {
+        id: 2,
+        name: "Under 50"
+      },
+      {
+        id: 3,
+        name: "Under 100"
+      },
+      {
+        id: 4,
+        name: "Under 500"
+      }
+    ],
+        })
+    }
+
+  </script>
